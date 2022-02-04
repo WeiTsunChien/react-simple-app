c26685efd2d37af1f83c342e702492e32dc64a34
wei-tsun-chien <dcr012345@gmail.com>
2022-02-04 19:13:25 +0800

    靜態切版

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,92 +3,104 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => {
     return (
         <>
-            <header className="navbar navbar-expand site-header">
+            <header className="site-header navbar navbar-expand fixed-top">
                 <i className="fas fa-bars" title="主選單"></i>
                 <a className="navbar-brand" href="#">React Simple App</a>
             </header>
-            <nav className="navbar bg-light">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 1</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 2</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 3</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 1</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 2</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 3</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 1</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 2</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 3</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 1</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 2</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 3</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 1</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 2</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 3</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 1</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 2</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 3</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 1</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 2</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 3</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 1</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 2</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link 3</a>
-                    </li>
-                </ul>
-            </nav>
+            <div className="sidebar-wrapper">
+                <nav className="sidebar navbar bg-light">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 1111</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 2</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 3</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 1</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 2</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 3</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 1</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 2</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 3</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 1</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 2</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 3</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 1</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 2</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 3</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 1</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 2</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 3</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 9</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 99</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 999</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 9999</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 99999</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link 999999</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="sidebar-overlay"></div>
+            </div>
             <div className="main-wrapper">
-                <h2>h2</h2>
-                <main>
+                <ul className="breadcrumb">
+                    <li className="breadcrumb-item"><a href="#">Photos</a></li>
+                    <li className="breadcrumb-item"><a href="#">Summer 2017</a></li>
+                    <li className="breadcrumb-item"><a href="#">Italy</a></li>
+                    <li className="breadcrumb-item active">Rome</li>
+                </ul>
+                <main className="container-fluid">
+                    <h2>h2</h2>
                     <Outlet />
                 </main>
             </div>
+            <footer>
+                footer
+            </footer>
         </>
     );
 };
