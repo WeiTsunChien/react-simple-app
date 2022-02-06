9653f9c81d51d80664301c86ebc91a947d95ab6b
wei-tsun-chien <dcr012345@gmail.com>
2022-02-06 21:44:32 +0800

    首頁

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,14 @@
 const Home = () => {
     return (
-        <>
-            Home
-        </>
+        <div className="row">
+            <div className="col-lg-6">
+                <div class="card bg-primary text-white mb-3">
+                    <div class="card-header">State待辦事項</div>
+                    <div class="card-body">todo list功能 (寫在 state中)</div>
+                    <div class="card-footer text-right">2022/02/06</div>
+                </div>
+            </div>
+        </div>
     );
 };
 
