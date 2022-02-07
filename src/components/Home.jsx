3c1f34181e70f52d52639880a8e6b5054e49b632
Edward <44120020+WeiTsunChien@users.noreply.github.com>
2022-02-07 18:28:43 +0800

    className

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,10 @@ const Home = () => {
     return (
         <div className="row">
             <div className="col-lg-6">
-                <div class="card bg-primary text-white mb-3">
-                    <div class="card-header">State待辦事項</div>
-                    <div class="card-body">todo list功能 (寫在 state中)</div>
-                    <div class="card-footer text-right">2022/02/06</div>
+                <div className="card mb-3">
+                    <div className="card-header">State待辦事項</div>
+                    <div className="card-body">todo list功能 (寫在 state中)</div>
+                    <div className="card-footer text-right">2022/02/06</div>
                 </div>
             </div>
         </div>
