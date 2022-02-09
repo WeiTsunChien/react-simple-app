5aca0ca780b0bebea6edbd2a2b7dd210a545282e
Edward <44120020+WeiTsunChien@users.noreply.github.com>
2022-02-09 14:56:23 +0800

    尚無待辦事項

--- src/components/StateTodo/List.jsx
+++ src/components/StateTodo/List.jsx
@@ -9,36 +9,37 @@ const List = (props) => {
     }, 0);
 
     return (
-        <div className="table-responsive-sm">
-            <table className="table table-bordered">
-                <thead>
-                    <tr className="table-primary">
-                        <th>是否完成</th>
-                        <th>事項</th>
-                        <th>功能</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        todos.map((todo) => {
-                            return (
-                                <Item key={todo.id}
-                                    {...todo}
-                                    updateTodo={updateTodo}
-                                    deleteTodo={deleteTodo} />
-                            )
-                        })
-                    }
-                </tbody>
-                <tfoot>
-                    <tr className="table-danger text-right">
-                        <td colSpan={3}>
-                            已經完成: {doneCount}, 總數: {todos.length}
-                        </td>
-                    </tr>
-                </tfoot>
-            </table>
-        </div>
+        !todos.length ? <div class="alert alert-info">尚無待辦事項</div> :
+            (<div className="table-responsive-sm">
+                <table className="table table-bordered">
+                    <thead>
+                        <tr className="table-primary">
+                            <th>是否完成</th>
+                            <th>事項</th>
+                            <th>功能</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            todos.map((todo) => {
+                                return (
+                                    <Item key={todo.id}
+                                        {...todo}
+                                        updateTodo={updateTodo}
+                                        deleteTodo={deleteTodo} />
+                                )
+                            })
+                        }
+                    </tbody>
+                    <tfoot>
+                        <tr className="table-danger text-right">
+                            <td colSpan={3}>
+                                已經完成: {doneCount}, 總數: {todos.length}
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>)
     );
 };
 
